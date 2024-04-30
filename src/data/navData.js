f8f3715980fc1d4c52c598395820792e56e581a0
Batrinath-Code <batrinath@outlook.com>
2024-04-30 08:51:54 +0530

    first commit

---
+++ src/data/navData.js
@@ -0,0 +1,20 @@
+const navData = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "About",
+    path: "#about",
+  },
+  {
+    name: "Product",
+    path: "#product",
+  },
+  {
+    name: "Contact",
+    path: "#contact",
+  },
+];
+
+export default navData;
